bd2a9ce81345603a094a7160b7a4af011ca405fe
Max Paris <maxp@partner.co>
2025-05-21 15:58:55 -0600

    hooks

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,11 +1,9 @@
-'use client';
-import {useRouter, useParams} from "next/navigation";
-
-export default function PropertyPage() {
-    const router = useRouter()
-    const params = useParams()
-    // params is an object that contains the dynamic segments of the URL
-    // for example, if the URL is /properties/123, params will be { id: '123' }
-
-    return (<div>Property Page { params.id } </div>)
+export default function PropertyPage({params, searchParams}) {
+  return (
+    <>
+        <h2>Property Page</h2>
+        <h2>Property ID: {params.id}</h2>
+        <h2>Property Name: {searchParams.name}</h2>
+    </>
+  )
 }
