fbdc017e44ea72f2418d93bccbbeea750a91f0c6
Maximiliano Paris <mparis@hotmail.cl>
2025-05-29 13:55:25 -0600

    feat: implement PropertyPage with database connection and add PropertyHeaderImage component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,9 +1,33 @@
-export default function PropertyPage({params, searchParams}) {
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+
+export default async function PropertyPage({params, searchParams}) {
+  await connectDB();
+  const property = await Property.findById(params.id).lean()
+
   return (
     <>
-        <h2>Property Page</h2>
-        <h2>Property ID: {params.id}</h2>
-        <h2>Property Name: {searchParams.name}</h2>
+      <PropertyHeaderImage image={property.images[0]}/>
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className={`mr-2`}/> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            {/*  Property Info */}
+          </div>
+        </div>
+      </section>
     </>
   )
 }
